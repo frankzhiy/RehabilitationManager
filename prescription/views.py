c6dd75458f65845b7b14387a5dc3f2a04055bc1a
frankzhiy <342323198@qq.com>
2025-04-19 09:50:59 +0800

    1.2.2 增加性别

--- prescription/views.py
+++ prescription/views.py
@@ -234,7 +234,7 @@ def get_motion_prescriptions(request):
         }
 
         other_data = list(other_prescriptions.values(
-            'name', 'id_card', 'phone', 'doctor', 'limbPrescription', 'pefPrescription',
+            'name', 'id_card', 'phone', 'doctor', 'limbPrescription', 'pefPrescription','sex'
             'respiratoryPrescription', 'swallowPrescription', 'tcePrescription', 'upload_time', 'is_active'
         ))
 
